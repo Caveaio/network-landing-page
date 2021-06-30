f8c23d865c4d51f0cd08de69e62371622e6334aa
piijt <piijt@users.noreply.github.com>
2021-06-30 14:30:54 +0000

    deploy: 7328d0e7505b37b816439cb8ffd7af1e9e5a1d37

--- _nuxt/static/1625063345/terms/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnetwrk\u002F_nuxt\u002Fstatic\u002F1625063345",layout:"default",error:a,serverRendered:true,routePath:"\u002Fterms",config:{_app:{basePath:"\u002Fnetwrk\u002F",assetsPath:"\u002Fnetwrk\u002F_nuxt\u002F",cdnURL:a}}}}(null));
