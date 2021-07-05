5ddbc77705520fd27d546e7b473acde18efb990f
RasmusRow <RasmusRow@users.noreply.github.com>
2021-07-05 14:41:20 +0000

    deploy: 4fe9f4af9e8a0585ae57362464ee7cc11f0fa759

--- _nuxt/static/1625495986/policy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnetwork-landing-page\u002F_nuxt\u002Fstatic\u002F1625495986",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpolicy",config:{_app:{basePath:"\u002Fnetwork-landing-page\u002F",assetsPath:"\u002Fnetwork-landing-page\u002F_nuxt\u002F",cdnURL:a}}}}(null));
